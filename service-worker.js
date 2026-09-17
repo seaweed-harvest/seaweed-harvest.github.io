c07887fe56cac9a92e84b27e1c0e9f09cc6775c3
bosunjm-cloud <bosunjm@gmail.com>
2026-09-18 06:49:52 +0800

    Refresh cache for Nursery Seaweed delete controls

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "seaweed-harvest-collection-v140-reef-seaweed-records";
+const CACHE_VERSION = "seaweed-harvest-collection-v141-reef-seaweed-delete";
 const NETWORK_TIMEOUT_MS = 5000;
 const APP_SHELL = [
   "./",
