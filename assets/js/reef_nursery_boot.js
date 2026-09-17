f3ec22a3ae8a602dea3a29cf01da63a0cdb31e8c
bosunjm-cloud <bosunjm@gmail.com>
2026-09-17 19:05:55 +0800

    Wire Reef site capture into production boot

--- assets/js/reef_nursery_boot.js
+++ assets/js/reef_nursery_boot.js
@@ -16,13 +16,17 @@ export const REEF_CANONICAL_ROUTE_PARAMETERS = Object.freeze([
 
 const parameters = new URLSearchParams(window.location.search);
 const reviewMode = Boolean(parameters.get("share") && parameters.get("org"));
+const legacySeaweedRecordRequested = Boolean(parameters.get("seaweed_record"));
 
 if (!reviewMode) {
   await Promise.all([
-    import("./reef_nursery_seaweed.js?v=1"),
+    legacySeaweedRecordRequested
+      ? import("./reef_nursery_seaweed.js?v=1")
+      : import("./reef_nursery_site_capture.js?v=20260917a"),
     import("./reef_nursery_inspection.js?v=2")
   ]);
 }
 
+await import("./reef_nursery_workspace_tabs.js?v=20260917a");
 await import("./reef_nursery_delete_hotfix.js?v=1");
 await import("./reef_nursery_cleanup.js?v=1");
